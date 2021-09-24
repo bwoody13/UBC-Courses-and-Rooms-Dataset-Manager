59188738fc0fa9026e3e2878bb320c1c46a88095
Matthew Wilson <matt.wilson720@gmail.com>
2021-09-24 15:50:12 -0700

    Removed eslint-disable line

--- test/resources/TestUtil.ts
+++ test/resources/TestUtil.ts
@@ -2,10 +2,9 @@ import * as fs from "fs-extra";
 
 const persistDir = "./data";
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-function getContentFromArchives(name: String): string {
-	return fs.readFileSync(`test/resources/archives/${name}`).toString("base64");
-}
+const getContentFromArchives = (name: string) =>
+	fs.readFileSync(`test/resources/archives/${name}`).toString("base64");
+
 
 function clearDisk(): void {
 	fs.removeSync(persistDir);
